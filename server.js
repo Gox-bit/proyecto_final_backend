8b3acb896c7b774d0389e1f33199e0a44f1463dd
Gox-bit <xavilesq@gmail.com>
2025-10-19 22:36:45 -0500

    Creación del servidor inicial con dependencias instaladas

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Servidor Express funcionando');
+});
+
+
+
+const PORT = process.env.PORT || 1578;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
