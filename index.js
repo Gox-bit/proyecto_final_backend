e05d32153560638a185e3a51b5b80dd9270006a4
alidali-star <jisminaa10@gmail.com>
2025-10-25 10:28:44 -0500

    React agregado

--- index.js
+++ index.js
@@ -2,20 +2,17 @@ const express = require("express");
 const router = express.Router();
 const Game = require("./models/Game");
 
-// Obtener todos los juegos
 router.get("/games", async (req, res) => {
   const games = await Game.find();
   res.json(games);
 });
 
-// Crear un nuevo juego
 router.post("/games", async (req, res) => {
   const newGame = new Game(req.body);
   await newGame.save();
   res.json({ message: "Juego agregado", newGame });
 });
 
-// Actualizar un juego (PUT)
 router.put("/games/:id", async (req, res) => {
   const updatedGame = await Game.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
@@ -23,7 +20,6 @@ router.put("/games/:id", async (req, res) => {
   res.json({ message: "Juego actualizado", updatedGame });
 });
 
-// Eliminar un juego
 router.delete("/games/:id", async (req, res) => {
   await Game.findByIdAndDelete(req.params.id);
   res.json({ message: "Juego eliminado" });
